728a90234531d2fe696c6310fdbacd8aa3b95b79
Student <amywieland19@gmail.com>
2021-01-29 00:08:16 -0600

    version1: output not entirely correct

--- assignment_0.c
+++ assignment_0.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #define ARRAY_SIZE 25
 void simulate(int table[ARRAY_SIZE][ARRAY_SIZE]);
+void tableprint(int table[ARRAY_SIZE][ARRAY_SIZE]);
 
 int main (int argc, char *argv[]){
-  int table[ARRAY_SIZE][ARRAY_SIZE];
+  int table[ARRAY_SIZE][ARRAY_SIZE] = {0};
 
   if(argc > 1){
-    if(!strcmp "--fps", argc[1]){
-     int fps = atoi(argv[2]);
+    if(!strcmp("--fps", argv[1])){
+      // int fps = atoi(argv[2]);
+     int j = 3;
+     while (j<argc){
+       table[atoi(argv[j])][atoi(argv[j+1])] = atoi(argv[j+2]);
+       j+=3;
+     }
     }
     
-  int i = 3
-  while (i<argc){
-    
-    
-    
+    int i = 1;
+    while (i<argc){
+      table[atoi(argv[i])][atoi(argv[i+1])] = atoi(argv[i+2]);
+      i+=3;
+    }
+  }
+ 
   //setting bounds of the table = to -1
   
   int j;
@@ -33,32 +42,39 @@ int main (int argc, char *argv[]){
 
   simulate(table);
   
+  return 0;
 }
 
 void tableprint(int table[ARRAY_SIZE][ARRAY_SIZE]){
   int j, i;
-  for (j = 0; j < ARRAY_SIZE; j++){
-    for (i = 0; i < ARRAY_SIZE; i++){
-      printf("%d",table[j][i]);
+  for (j = 1; j < ARRAY_SIZE-1 ; j++){
+    printf("\n");
+    for (i = 1; i < ARRAY_SIZE-1 ; i++){
+      if (table[j][i] == -1){
+	printf(" #");
+      }
+      else{
+      printf(" %d",table[j][i]);
+      }
     }
   }
-  
 }
 
 void simulate(int table[ARRAY_SIZE][ARRAY_SIZE]){
 
-  int center = ARRAY_SIZE/2
+  int center = ARRAY_SIZE/2;
 
   table[center][center]++;
 
   int i;
   int j;
-  boolean topple = true; //signals whether a topple has occurred or not
+  bool topple = true; //signals whether a topple has occurred or not
 
   while (topple){
     topple = false;
   for (j = 0; j < ARRAY_SIZE; j++){
-    for (i = 0; j < ARRAY_SIZE; i++){
+    tableprint(table);
+    for (i = 0; i < ARRAY_SIZE; i++){
       if (table[j][i] > 8){
 	table[j][i] = 1;
 	topple = true;
